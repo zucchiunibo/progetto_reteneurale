708728e9df7572f8ca8489c7bcecf5ca58aa471a
Simone Zucchi <simone.zucchi2@studio.unibo.it>
2025-05-08 10:59:05 +0200

    added branch zucco

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
-#include <iostream>
+#include <SFML/Graphics/Image.hpp>
 
 int main() {
-    std::cout << "Hi\n";
+  sf::Image orecchino;
+  if (!orecchino.loadFromFile("orecchino.png"))
+    return -1;
+
+  sf::Color color = orecchino.getPixel(0, 0);
+  color.a         = 0;
+  orecchino.setPixel(0, 0, color);
+  if (!orecchino.saveToFile("result.png"))
+    return -1;
 }
