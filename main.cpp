66994c1980fc41868b5fd508f3ab8d994d01751d
Simone Zucchi <simone.zucchi2@studio.unibo.it>
2025-05-13 11:18:37 +0200

    main now modifies images (replaces all pixels with colored stripes)

--- main.cpp
+++ main.cpp
@@ -1,13 +1,33 @@
 #include <SFML/Graphics/Image.hpp>
+#include <iostream>
+#include <vector>
 
 int main() {
   sf::Image orecchino;
-  if (!orecchino.loadFromFile("orecchino.png"))
+  if (!orecchino.loadFromFile("./orecchino.png"))
     return -1;
+  auto pixelArray{orecchino.getPixelsPtr()};
+  auto size = orecchino.getSize();
+  auto n    = size.x * size.y * 4;
+  std::vector<sf::Color> pixels{};
+  for (int i{0}; i < n / 4; i += 4) {
+    pixels.push_back({pixelArray[i], pixelArray[i + 1], pixelArray[i + 2],
+                      pixelArray[i + 3]});
+  }
 
-  sf::Color color = orecchino.getPixel(0, 0);
-  color.a         = 0;
-  orecchino.setPixel(0, 0, color);
-  if (!orecchino.saveToFile("result.png"))
+  for (int i{0}; i < pixels.size() / 4; i += 4) {
+    pixels[i]     = sf::Color::Yellow;
+    pixels[i + 1] = sf::Color::Red;
+    pixels[i + 2] = sf::Color::Cyan;
+    pixels[i + 3] = sf::Color::White;
+  }
+
+  for (int i{0}; i < size.x; ++i) {
+    for (int j{0}; j < size.y; ++j) {
+      orecchino.setPixel(i, j, pixels[i]);
+    }
+  }
+
+  if (!orecchino.saveToFile("./result.png"))
     return -1;
 }
