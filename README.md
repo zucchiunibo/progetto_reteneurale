ab1939b18612adec5ff370359223078f748e3112
Simone Zucchi <simone.zucchi2@studio.unibo.it>
2025-05-22 19:58:51 +0200

    updated README and correctly saved first two patterns

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 Hello world!
+
+## CMake Instructions
+```bash
+# To compile Debug build
+cmake --build build --config Debug
+
+# To compile Release build
+cmake --build build --config Release
+
+# To run Debug build
+./build/Debug/progetto
+
+# To run Release build
+./build/Release/progetto
--- main.cpp
+++ main.cpp
@@ -4,14 +4,17 @@
 
 int main() {
   sf::Image inputImage;
-  if (!inputImage.loadFromFile("./patterns/original/orecchino.png"))
+  if (!inputImage.loadFromFile("./patterns/original/ledzeppelin.jpg"))
     return -1;
 
   sf::Image resizedImage{img::resize(inputImage)};
   auto binaryVector{img::binaryConvert(resizedImage)};
 
   sf::Image binary(img::formImage(binaryVector));
+
+  if (!resizedImage.saveToFile("./patterns/resized/ledzeppelin.png"))
+    return -1;
   
-  if (!binary.saveToFile("./patterns/binary/result.png"))
+  if (!binary.saveToFile("./patterns/binary/ledzeppelin.png"))
     return -1;
 }
